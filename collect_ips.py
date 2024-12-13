192e76687709064a2060b91b297ea85ee4fbe4ba
yd072 <luiquirong@gmail.com>
2024-12-14 01:23:06 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -6,7 +6,7 @@ import os
 # 目标URL列表
 urls = ['https://ip.164746.xyz/ipTop10.html', 
         'https://api.uouin.com/cloudflare.html'
-        'http://ip.flares.cloud'
+        'https://stock.hostmonit.com/CloudFlareYes'
         ]
 
 # 正则表达式用于匹配IP地址
@@ -30,7 +30,9 @@ with open('ip.txt', 'w') as file:
             elements = soup.find_all('tr')
         elif url == 'https://api.uouin.com/cloudflare.html':
             elements = soup.find_all('tr')
-        else url == 'http://ip.flares.cloud':
+        elif url == 'https://stock.hostmonit.com/CloudFlareYes':
+            elements = soup.find_all('tr')
+        else:
             elements = soup.find_all('li')
         
         # 遍历所有元素,查找IP地址
